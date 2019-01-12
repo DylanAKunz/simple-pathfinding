685edd6703f79667102c0114b5d4077140d2d162
DylanAKunz <Dylan.a.Kunz@gmail.com>
2019-01-12 03:28:00 -0800

    Initial commit

---
+++ src/pathfinding.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class pathfinding {
+	public static final int dimension = 10;
+	public static final int obstacles = 5;
+	
+	//generate a series of obstacles for the pathfinding
+	/*
+	public static boolean[][] obstacleGenerate(int number){
+	}*/
+	
+	/*
+	//Travel along the path towards the destination
+	public static int[] pathfinding(int[] previousPosition, int direction) {}
+	*/
+	
+	//Initialize obstacles, call functions for pathfinding, display path.
+	public static void main(String[] args) {
+	}
+
+}
